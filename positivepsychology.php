b0cd3050d1ee28aacbd80c62491f6dc2b912a70f
Mohammad Haseeb Ashiq <learndotpk@gmail.com>
2022-03-02 09:40:14 +0500

    02/03/2022 03

--- positivepsychology.php
+++ positivepsychology.php
@@ -143,10 +143,12 @@
     </div>
     <div class="address-loc">
         <p><span><i class="fas fa-map-marker"></i> Address:</span> Ciputra, Hanoi, Vietnam </p>
-        <!-- <p><span><i class="fas fa-globe"></i> Website:</span> </p> -->
-        <p><span>Facebook:</span> <a href="https://www.facebook.com/New-Me-Holistic-Centre-104251028680304"><i class="fab fa-facebook" style="color:blue;"></i> </a></p>
-        <p><span><i class="fab fa-whatsapp" style="color:green;"></i> Whatsapp:+84 120 853 1811</span> </p>
-        <p><span><i class="fas fa-phone-square-alt" style="color:blue;"></i> Telephone:+84 976079523</span></p>
+        <p><span><i class="fas fa-phone-square-alt"></i> Telephone:+84 976079523</span></p>
+       <h3>Social Media</h3>
+        <div class="social-media-links">
+       <p><a href="https://www.facebook.com/New-Me-Holistic-Centre-104251028680304" target="_blank" id="f"><i class="fab fa-facebook" id="facebookLink"></i> </a></p>
+        <p><a href="https://wa.me/841208531811" target="_blank"><i class="fab fa-whatsapp" id="whatsappLink"></i></a> </p>
+       </div>
     </div>
 </div>
 
